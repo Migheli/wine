01e543fc5cacbfd5af2855c2aa509fd7f3483ea1
Migheli <98918370+Migheli@users.noreply.github.com>
2022-07-31 01:25:41 +0300

    Added Jinja2 functional
    Added Jinja2 functionality in project

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import datetime
+
+now = datetime.datetime.now()
+
+established = '1920'
+
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+rendered_page = template.render(
+    cap1_title="Красная кепка",
+    company_age=str(int(now.year) - int(established))
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
