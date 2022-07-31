57cbbd269a195e9c5907bfb3218c910fd10f5cd4
Migheli <98918370+Migheli@users.noreply.github.com>
2022-07-31 22:42:31 +0300

    Pep8 and optimizations

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
 import datetime
-from pprint import pprint
-
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from collections import defaultdict
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from pandas import read_excel
-
-established = 1920
-now = datetime.datetime.now()
-categorised_drinks = read_excel('wine2.xlsx', sheet_name='Лист1', keep_default_na=False)
-drinks = categorised_drinks.to_dict(orient='record')
-print(drinks)
-
-drinks_by_categories = defaultdict(list)
-
-for drink in drinks:
-    drink_category = drink['Категория']
-    drinks_by_categories[drink_category].append(drink)
-    drink.pop('Категория')
-
-pprint(drinks_by_categories)
 
 
 def year_notation_generator(year):
@@ -33,24 +16,42 @@ def year_notation_generator(year):
         year_notation = 'года'
     return year_notation
 
-company_age = str(now.year - established)
-year_notation = year_notation_generator(company_age)
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+def main():
+    established = 1920
+    now = datetime.datetime.now()
+    company_age = str(now.year - established)
+    year_notation = year_notation_generator(company_age)
+    categorised_drinks = read_excel(
+        'wine3.xlsx',
+        sheet_name='Лист1',
+        keep_default_na=False
+    )
+    drinks = categorised_drinks.to_dict(orient='record')
+    drinks_by_categories = defaultdict(list)
+    for drink in drinks:
+        drink_category = drink['Категория']
+        drinks_by_categories[drink_category].append(drink)
+        drink.pop('Категория')
 
-template = env.get_template('template.html')
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-rendered_page = template.render(
-    company_age=company_age,
-    year_notation=year_notation,
-    drinks_by_categories=drinks_by_categories
-)
+    template = env.get_template('template.html')
+    rendered_page = template.render(
+        company_age=company_age,
+        year_notation=year_notation,
+        drinks_by_categories=drinks_by_categories
+    )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
